d5c317a7335699db0ebb9dff0ca14449c077b708
treylewis18 <114061@new-haven.k12.ct.us>
2016-12-08 12:57:18 -0500

    Another attempt to update
    Work already

--- team1.py
+++ team1.py
@@ -6,6 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
+team_name = 'Please update already'  # Only 10 chars displayed - "100 crew" is desired name
 strategy_name = ''
 strategy_description = ''
     
