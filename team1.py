b20c57141a3f6ae21676e91d56d48fc74f1b8c24
treylewis18 <114061@new-haven.k12.ct.us>
2016-12-08 12:35:56 -0500

    Update team name
    That's it

--- team1.py
+++ team1.py
@@ -6,9 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = ''
+strategy_description = ''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
